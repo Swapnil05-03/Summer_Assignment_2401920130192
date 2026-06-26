348003ab7dd6c7675f15b26b027469c684348bef
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-06-26 10:26:38 +0530

    Added Week 4 Day 5 solutions

---
+++ Week_4/Day_5/ConstructBinaryTreeFromPreorderAndInorderTraversal/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = inorder.length;
+        return build(0 , n-1 , 0 , n-1 , inorder , preorder);
+    }
+
+    public static TreeNode build(int prelo , int prehi , int inlo , int inhi ,int inorder[], int preorder[]) {
+        if(prelo > prehi || inlo > inhi) return null;
+        int val = preorder[prelo];
+        TreeNode root = new TreeNode(val);
+        int r = -1;
+        for(int i=inlo ; i<=inhi; i++){
+            if(inorder[i] == val){
+                r = i;
+                break;
+            }
+        }
+        int cnt = r - inlo;
+        root.left = build(prelo+1 , prelo+cnt , inlo , r-1 , inorder , preorder);
+        root.right = build(prelo+cnt+1 , prehi , r+1 , inhi , inorder , preorder);
+        return root;
+    }
+}
---
+++ Week_4/Day_5/SymmetricTree/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        invertTree(root.left);
+        return isSameTree(root.left , root.right);
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
+        return isSameTree(p.left , q.left) && isSameTree(p.right , q.right);
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
